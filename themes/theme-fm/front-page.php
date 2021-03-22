0d953f3009b5471611d0ba8d4b696174f0f96645
manexonmorin <62068818+manexonmorin@users.noreply.github.com>
2021-03-22 10:01:08 -0400

    modification de la page web et mettre le carrousel dans le front-page.php.

--- themes/theme-fm/front-page.php
+++ themes/theme-fm/front-page.php
@@ -9,7 +9,16 @@
 
 get_header();
 ?>
-/////////////////////////////////////////////////////////////FRONT-PAGE.PHP
+		<section class="carrousel">
+			<div>Le futur</div>
+			<div>C'est </div>
+			<div>Le code !!</div>
+		</section>
+
+		<button id="un">1</button>
+		<button id="deux">2</button>
+		<button id="trois">3</button>
+
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -25,32 +34,21 @@ get_header();
 			<?php
 			/* Start the Loop */
             $precedent = "XXXXXX";
+			// global $tPropriété;
 			while ( have_posts() ) :
 				the_post();
-                $titre = get_the_title();
-				//582-1W1 Mise en page Web (75h)
-				$sigle = substr($titre, 0, 7);
-				$nbHeure = substr($titre, -4, 3);
-				$titrePartiel = substr($titre, 8, -6);
-                $session = substr($titre, 4, 1);
-                // $contenu = get_the_content();
-                // $resume = substr($contenu, 0, 200);
-				$typeCours = get_field('type_de_cours');
+				convertirTableau($tPropriété);
 
-				if ($typeCours != $precedent):
+				if ($tPropriété['typeCours'] != $precedent):
 					if("XXXXXX" != $precedent): ?>
 						</section> 
 					<?php endif?>
-				<h2><?php  echo $typeCours; ?></h2>
+				<h2><?php  echo $tPropriété['typeCours']; ?></h2>
 				<section>
 				<?php endif?>
-				<article>
-					<p><?php echo $sigle . " - " . $typeCours; ?></p>
-					<a href="<?php echo get_permalink() ?>"><?php echo $titrePartiel; ?></a>
-					<p>Session : <?php echo $session; ?></p>
-				</article>
+				<?php get_template_part( 'template-parts/content', 'cours-article' );?>
 			<?php 
-			$precedent = $typeCours;
+			$precedent = $tPropriété['typeCours'];
 			endwhile;?>
 			</section> <!--Fin section cours-->
 		<?php endif;?>
@@ -59,3 +57,25 @@ get_header();
 <?php
 get_sidebar();
 get_footer();
+
+function convertirTableau(&$tPropriété){
+
+	/*	
+	$titre = get_the_title();
+	//582-1W1 Mise en page Web (75h)
+	$sigle = substr($titre, 0, 7);
+	$nbHeure = substr($titre, -4, 3);
+	$titrePartiel = substr($titre, 8, -6);
+	$session = substr($titre, 4, 1);
+	// $contenu = get_the_content();
+	// $resume = substr($contenu, 0, 200);
+	$typeCours = get_field('type_de_cours');
+*/
+
+	$tPropriété['titre'] = get_the_title();
+	$tPropriété['sigle'] = substr($tPropriété['titre'], 0, 7);
+	$tPropriété['nbHeure'] = substr($tPropriété['titre'], -4, 3);
+	$tPropriété['titrePartiel'] = substr($tPropriété['titre'], 8, -6);
+	$tPropriété['session'] = substr($tPropriété['titre'], 4, 1);
+	$tPropriété['typeCours'] = get_field('type_de_cours');
+}
---
+++ themes/theme-fm/template-parts/content-cours-article.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part l'affichage des blocs de cours dans le front page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme-FM
+ */
+global $tPropriété;
+?>
+
+<article>
+	<p><?php echo $tPropriété['sigle'] . " - " . $tPropriété['typeCours'] . "-" . $tPropriété['nbHeure']; ?></p>
+	<a href="<?php echo get_permalink() ?>"><?php echo $tPropriété['titrePartiel']; ?></a>
+	<p>Session : <?php echo $tPropriété['session']; ?></p>
+</article>
